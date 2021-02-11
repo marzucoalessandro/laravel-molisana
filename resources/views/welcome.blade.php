f2ea7867eba0b2aedf8c759b233a7f7790a8af4a
Alessandro <marzucoalessandro1@gmail.com>
2021-02-11 16:01:03 +0100

    inizio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/app.css">
+    <title>La molisana</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  </head>
+  <body>
+    <!-- costruisco il sito che verrà poi destrutturato nei vari partials -->
+    <header>
+      <img src="{{asset ('img/logo.png')}}" alt="">
+    </header>
+    <nav>
+      @foreach ($link as $item)
+      <a href="#">{{$item}}</a>
+      @endforeach
+    </nav>
+    <main>
+      
+    </main>
+  </body>
+  </html>
